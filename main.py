f2164f6a4cb490264495633d6af3028eb9901069
Sasha <svsasha@tut.by>
2021-09-09 01:11:11 +0300

    add task_1

---
+++ main.py
@@ -0,0 +1,62 @@
+# print(1)
+# print(2)
+#
+# a =6
+# if a >3:
+#     print("a=", a)
+
+# name=input()
+# print(name)
+a='sash'
+b=1
+c=1.1
+d=bytes([46, 46, 46])
+list=['Al', 'Bul', 'Ci']
+tuple=(1, 1.1, 'CVN')
+set={'h', 'o', 'l', 'e'}
+frozenset=frozenset(['h', 'o', 'l', 'e', 'e'])
+dict={1:'Ola', 2:'Hello'}
+
+print('a= ', a, type(a))
+print('b= ', b, type(b))
+print('c= ', c, type(c))
+print('d= ', d, type(d))
+print('list= ', list, type(list))
+print('tuple= ', tuple, type(tuple))
+print('set= ', set, type(set))
+print('frozenset= ', frozenset, type(frozenset))
+print('dict= ', dict, type(dict))
+
+a='1'
+b='2'
+c=a+b
+print('c= ', c, type(c))
+
+a=1
+b=2
+c=a+b
+print('c= ', c, type(c))
+
+a=10
+b=6
+c=a/b
+print('c= ', c, type(c))
+
+a=10
+b=2
+c=a*b
+print('c= ', c, type(c))
+
+a=10
+b=6
+c=a//b
+print('c= ', c, type(c))
+
+a=10
+b=6
+c=a%b
+print('c= ', c, type(c))
+
+
+
+
